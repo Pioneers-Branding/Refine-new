d9bb0894108a14323177537b47f1bfaa3b397f83
anshrealestate <anshrealestate@gmail.com>
2026-07-17 16:20:47 +0530

    Add Wacrs widget script to scripts.php

--- includes/scripts.php
+++ includes/scripts.php
@@ -7,3 +7,6 @@
 
 <!-- Main Application JS -->
 <script src="assets/js/main.js"></script>
+
+<!-- Wacrs Widget -->
+<script defer src="https://app.wacrs.com/install-widget/bundle.js?key=9474e4a1-19e1-4497-9c13-79a0b04c2956"></script>
