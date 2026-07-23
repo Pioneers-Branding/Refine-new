c364edae20e9872daa5eed3e1aedb46d257512aa
anshrealestate <anshrealestate@gmail.com>
2026-07-23 14:48:25 +0530

    Enable error reporting in blog.php to diagnose live server 500 error

--- blog.php
+++ blog.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 // Determine path and slug for routing
 $requestUri = $_SERVER['REQUEST_URI'] ?? '/blog';
 $parsedPath = parse_url($requestUri, PHP_URL_PATH);
